8cd1148ec8174821a6094c949691c6ca71238829
k.sliz <kinga.pawliszyn@gmail.com>
2020-06-04 13:34:23 +0200

    added map, placeholder statistics and form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'my-app';
+
+  readonly options = ['Opcja 1', 'Opcja 2', 'Opcja 3'];
+
+  sideNavForm = new FormGroup({
+    firstValue: new FormControl('', Validators.required),
+    optionValue: new FormControl('', Validators.required),
+    checkboxValue: new FormControl('')
+  });
+
+  constructor(private snackBar: MatSnackBar) { }
+
+
+
+  submit() {
+    let controlsValuesMsg = `Wprowadzone wartości: `;
+
+    for (const key in this.sideNavForm.controls) {
+      if (this.sideNavForm.controls.hasOwnProperty(key)) {
+        const element = this.sideNavForm.controls[key]?.value;
+        controlsValuesMsg += element;
+        if (key !== 'checkboxValue') {
+          controlsValuesMsg += ', ';
+        }
+      }
+    }
+
+    this.snackBar.open(controlsValuesMsg, 'Ok', {
+      duration: 2000,
+    });
+
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatCardModule } from '@angular/material/card';
+import { PieChartComponent } from './pie-chart/pie-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PieChartComponent
   ],
   imports: [
-    BrowserModule
+    CommonModule,
+    FormsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatSnackBarModule,
+    MatSliderModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
